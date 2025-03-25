c08022696386bceb6c109fdfa6d4988659b9dc79
abineshPalanisamy <abinesh.palanisamy@syncfusion.com>
2025-03-25 10:31:11 +0530

    KB changes

---
+++ lib/main.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:collection/collection.dart';
+
+void main() {
+  runApp(const MaterialApp(home: SfDataGridDemo()));
+}
+
+class SfDataGridDemo extends StatefulWidget {
+  const SfDataGridDemo({super.key});
+
+  @override
+  SfDataGridDemoState createState() => SfDataGridDemoState();
+}
+
+class SfDataGridDemoState extends State<SfDataGridDemo> {
+  List<Employee> _employees = <Employee>[];
+  late EmployeeDataSource _employeeDataSource;
+  DataGridController dataGridController = DataGridController();
+  late CustomSelectionManager customSelectionManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _employees = getEmployeeData();
+    _employeeDataSource = EmployeeDataSource(_employees, context);
+    customSelectionManager = CustomSelectionManager(dataGridController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Syncfusion Flutter DataGrid')),
+      body: SfDataGrid(
+        source: _employeeDataSource,
+        controller: dataGridController,
+        allowEditing: true,
+        selectionManager: customSelectionManager,
+        navigationMode: GridNavigationMode.cell,
+        selectionMode: SelectionMode.single,
+        columns: <GridColumn>[
+          GridColumn(
+            columnName: 'id',
+            label: Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.centerRight,
+              child: const Text('ID'),
+            ),
+          ),
+          GridColumn(
+            columnName: 'name',
+            label: Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.centerLeft,
+              child: const Text('Name'),
+            ),
+          ),
+          GridColumn(
+            allowEditing: false,
+            columnName: 'designation',
+            label: Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.centerLeft,
+              child: const Text('Designation', overflow: TextOverflow.ellipsis),
+            ),
+          ),
+          GridColumn(
+            columnName: 'salary',
+            label: Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.centerRight,
+              child: const Text('Salary'),
+            ),
+          ),
+        ],
+        columnWidthMode: ColumnWidthMode.fill,
+      ),
+    );
+  }
+}
+
+class CustomSelectionManager extends RowSelectionManager {
+  CustomSelectionManager(this.dataGridController);
+  DataGridController dataGridController;
+
+  @override
+  Future<void> handleKeyEvent(KeyEvent keyEvent) async {
+    if (keyEvent.logicalKey == LogicalKeyboardKey.enter) {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        dataGridController.beginEdit(dataGridController.currentCell);
+      });
+    }
+    super.handleKeyEvent(keyEvent);
+  }
+}
+
+class EmployeeDataSource extends DataGridSource {
+  EmployeeDataSource(this.employees, this.buildContext) {
+    dataGridRows =
+        employees
+            .map<DataGridRow>((dataGridRow) => dataGridRow.getDataGridRow())
+            .toList();
+  }
+
+  List<Employee> employees = [];
+
+  List<DataGridRow> dataGridRows = [];
+
+  BuildContext buildContext;
+
+  /// Helps to hold the new value of all editable widget.
+  /// Based on the new value we will commit the new value into the corresponding
+  /// [DataGridCell] on [onSubmitCell] method.
+  dynamic newCellValue;
+
+  /// Help to control the editable text in [TextField] widget.
+  TextEditingController editingController = TextEditingController();
+
+  @override
+  List<DataGridRow> get rows => dataGridRows;
+
+  @override
+  DataGridRowAdapter? buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+      cells:
+          row.getCells().map<Widget>((dataGridCell) {
+            return Container(
+              alignment:
+                  (dataGridCell.columnName == 'id' ||
+                          dataGridCell.columnName == 'salary')
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                dataGridCell.value.toString(),
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          }).toList(),
+    );
+  }
+
+  @override
+  Future<void> onCellSubmit(
+    DataGridRow dataGridRow,
+    RowColumnIndex rowColumnIndex,
+    GridColumn column,
+  ) async {
+    final dynamic oldValue =
+        dataGridRow
+            .getCells()
+            .firstWhereOrNull(
+              (DataGridCell dataGridCell) =>
+                  dataGridCell.columnName == column.columnName,
+            )
+            ?.value ??
+        '';
+
+    final int dataRowIndex = dataGridRows.indexOf(dataGridRow);
+
+    if (newCellValue == null || oldValue == newCellValue) {
+      return;
+    }
+
+    if (column.columnName == 'id') {
+      dataGridRows[dataRowIndex].getCells()[rowColumnIndex.columnIndex] =
+          DataGridCell<int>(columnName: 'id', value: newCellValue);
+      employees[dataRowIndex].id = newCellValue as int;
+    } else if (column.columnName == 'name') {
+      dataGridRows[dataRowIndex].getCells()[rowColumnIndex.columnIndex] =
+          DataGridCell<String>(columnName: 'name', value: newCellValue);
+      employees[dataRowIndex].name = newCellValue.toString();
+    } else if (column.columnName == 'designation') {
+      dataGridRows[dataRowIndex].getCells()[rowColumnIndex.columnIndex] =
+          DataGridCell<String>(columnName: 'designation', value: newCellValue);
+      employees[dataRowIndex].designation = newCellValue.toString();
+    } else {
+      dataGridRows[dataRowIndex].getCells()[rowColumnIndex.columnIndex] =
+          DataGridCell<int>(columnName: 'salary', value: newCellValue);
+      employees[dataRowIndex].salary = newCellValue as int;
+    }
+  }
+
+  @override
+  Widget? buildEditWidget(
+    DataGridRow dataGridRow,
+    RowColumnIndex rowColumnIndex,
+    GridColumn column,
+    CellSubmit submitCell,
+  ) {
+    // Text going to display on editable widget
+    final String displayText =
+        dataGridRow
+            .getCells()
+            .firstWhereOrNull(
+              (DataGridCell dataGridCell) =>
+                  dataGridCell.columnName == column.columnName,
+            )
+            ?.value
+            ?.toString() ??
+        '';
+
+    // The new cell value must be reset.
+    // To avoid committing the [DataGridCell] value that was previously edited
+    // into the current non-modified [DataGridCell].
+    newCellValue = null;
+
+    final bool isNumericType =
+        column.columnName == 'id' || column.columnName == 'salary';
+
+    // Holds regular expression pattern based on the column type.
+    final RegExp regExp = _getRegExp(isNumericType, column.columnName);
+
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      alignment: isNumericType ? Alignment.centerRight : Alignment.centerLeft,
+      child: TextField(
+        controller: editingController..text = displayText,
+        textAlign: isNumericType ? TextAlign.right : TextAlign.left,
+        autofocus: true,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 8.0),
+        ),
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.allow(regExp),
+        ],
+        keyboardType: isNumericType ? TextInputType.number : TextInputType.text,
+        onChanged: (String value) {
+          if (value.isNotEmpty) {
+            if (isNumericType) {
+              newCellValue = int.parse(value);
+            } else {
+              newCellValue = value;
+            }
+          } else {
+            newCellValue = null;
+          }
+        },
+        onSubmitted: (String value) {
+          submitCell();
+        },
+      ),
+    );
+  }
+
+  RegExp _getRegExp(bool isNumericKeyBoard, String columnName) {
+    return isNumericKeyBoard ? RegExp('[0-9]') : RegExp('[a-zA-Z ]');
+  }
+}
+
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
+
+  int? id;
+  String? name;
+  String? designation;
+  int? salary;
+
+  DataGridRow getDataGridRow() {
+    return DataGridRow(
+      cells: <DataGridCell>[
+        DataGridCell<int>(columnName: 'id', value: id),
+        DataGridCell<String>(columnName: 'name', value: name),
+        DataGridCell<String>(columnName: 'designation', value: designation),
+        DataGridCell<int>(columnName: 'salary', value: salary),
+      ],
+    );
+  }
+}
+
+List<Employee> getEmployeeData() {
+  return [
+    Employee(10001, 'Lara', 'Project Lead', 45000),
+    Employee(10002, 'James', 'Developer', 23000),
+    Employee(10003, 'Kathryn', 'Project Lead', 43500),
+    Employee(10004, 'Michael', 'Manager', 50000),
+    Employee(10005, 'Martin', 'Developer', 25000),
+    Employee(10006, 'Jack', 'Project Lead', 54000),
+    Employee(10007, 'Balnc', 'UI Designer', 28500),
+    Employee(10008, 'Lara', 'Project Lead', 40000),
+    Employee(10009, 'James', 'Developer', 23000),
+    Employee(10010, 'Kathryn', 'Project Lead', 43500),
+  ];
+}
